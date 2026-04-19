4bd19ff57dd40c3438a9c5e86b5ee9f4fd6038d6
Maksym <72688794+MaksIMMum@users.noreply.github.com>
2026-04-19 18:09:19 +0300

    feat: implement solution for LeetCode 450: Delete Node in a BST
    Implemented by restructuring the bst. the search for the key is simple tree traverse. If we find the key, we check for the two cases with no children, and one child, which is handled by returning the other, and if 0 children it's just None. If two children, we traverse the right tree, and look for the smallest element, providing that it's smaller than the whole right tree and bigger than the left side elements, we can do that. then we just need to delete that element from the right tree which is why we call the deleteNode again to do the same thing over.

---
+++ 450 Delete Node in a BST/450 Delete Node in a BST.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+    def deleteNode(self, root, key):
+        """
+        :type root: Optional[TreeNode]
+        :type key: int
+        :rtype: Optional[TreeNode]
+        """
+        if not root:
+            return root
+        if key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            if not root.left:
+                return root.right
+            if not root.right:
+                return root.left
+
+            node = root.right
+            while node.left:
+                node = node.left
+            root.val = node.val
+            root.right = self.deleteNode(root.right, root.val)
+        return root
